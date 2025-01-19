1da1b317a4e8c52bb7aa9c17648e518567b596a5
Justin Ng <Vivoe@users.noreply.github.com>
2025-01-19 01:46:24 +0000

    Adding multi-device support.

--- light.py
+++ light.py
@@ -1,15 +1,15 @@
 """YN360 Light integration for Home Assistant."""
 
+import asyncio
 import logging
 
-from bleak import BleakClient
+from bleak import BleakClient, BleakError
 
-from homeassistant.components.bluetooth import async_ble_device_from_address
 from homeassistant.components.light import LightEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN, PAYLOAD_FLUSH, PAYLOAD_OFF, PAYLOAD_ON_DEFAULT
+from .const import DOMAIN, PAYLOAD_OFF, PAYLOAD_ON_DEFAULT
 
 LOGGER = logging.getLogger(__name__)
 
@@ -18,10 +18,11 @@ async def async_setup_entry(
     hass: HomeAssistant, config_entry: ConfigEntry, async_add_entities
 ) -> bool:
     """Set up the YN360 light platform."""
-    config = hass.data[DOMAIN][config_entry.entry_id]
+    entry_data = hass.data[DOMAIN][config_entry.entry_id]
+    async_add_entities(YN360Light(entry_data))
 
-    ble_client = async_ble_device_from_address(hass, config["uuid"])
-    async_add_entities([YN360Light(ble_client, config["uuid"], config["control_uuid"])])
+    # ble_client = async_ble_device_from_address(hass, config["uuid"])
+    # async_add_entities([YN360Light(ble_client, config["uuid"], config["control_uuid"])])
     return True
 
 
@@ -34,23 +35,39 @@ async def async_remove_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
 class YN360Light(LightEntity):
     """YN360 light entity."""
 
-    def __init__(self, ble_client, uuid, control_uuid) -> None:
+    def __init__(self, entry_data) -> None:
         """Initialize the light."""
-        self._name = "YN360_" + uuid
-        self._uuid = uuid
+        self._entry_data = entry_data
+        self._name = "YN360_" + entry_data["uid"]
         self._state = False
-        self._ble_client = ble_client
-        self._control_uuid = control_uuid
         self._state_payload = PAYLOAD_ON_DEFAULT
 
-    async def send_payload(self, client, payloads):
+    async def get_client(self, address):
+        """Attempt to get a BleakClient, returns None if cannot connect."""
+        try:
+            return BleakClient(address, timeout=1).connect()
+        except BleakError:
+            return None
+
+    async def get_clients(self):
+        """Get all potential devices. Missing devices wil be filtered out."""
+        clients_all = [self.get_client(address) for address in self._entry_data["uuid"]]
+        return [client for client in clients_all if client is not None]
+
+    async def send_payload(self, clients, payloads):
         """Send a hex payload to the device."""
         if not isinstance(payloads, list):
             payloads = [payloads]
 
         for payload in payloads:
             data = bytes.fromhex(payload)
-            await client.write_gatt_char(self._control_uuid, data)
+            tasks = [
+                client.write_gatt_char(
+                    self._entry_data["control_uuid"][client.address], data
+                )
+                for client in clients
+            ]
+            await asyncio.gather(*tasks)
 
     @property
     def name(self):
@@ -59,17 +76,17 @@ class YN360Light(LightEntity):
 
     async def async_turn_on(self, **kwargs):
         """Turn on."""
-        async with BleakClient(self._ble_client) as client:
-            LOGGER.debug("Turning on with payload: %s", self._state_payload)
-            await self.send_payload(client, [PAYLOAD_FLUSH, self._state_payload])
-            self._state = True
+        clients = await self.get_clients()
+        LOGGER.debug("Turning on with payload: %s", self._state_payload)
+        await self.send_payload(clients, self._state_payload)
+        self._state = True
 
     async def async_turn_off(self, **kwargs):
         """Turn off."""
-        async with BleakClient(self._ble_client) as client:
-            LOGGER.debug("Turning off with payload: %s", PAYLOAD_OFF)
-            await self.send_payload(client, [PAYLOAD_FLUSH, PAYLOAD_OFF])
-            self._state = False
+        clients = await self.get_clients()
+        LOGGER.debug("Turning on with payload: %s", self._state_payload)
+        await self.send_payload(clients, PAYLOAD_OFF)
+        self._state = False
 
     def turn_on(self, **kwargs):
         """Turn on, but not implemented."""
@@ -87,4 +104,4 @@ class YN360Light(LightEntity):
     @property
     def unique_id(self):
         """Unique ID."""
-        return "yn360_" + self._uuid
+        return self._entry_data["uid"]
